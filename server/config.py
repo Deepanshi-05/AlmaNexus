aab9564e8d766a94fee82385b397f888fc8cdf4e
Deepanshi Singh <158538244+GitNinja-bcd@users.noreply.github.com>
2025-04-19 10:13:41 +0530

    Your commit message describing the change

--- server/config.py
+++ server/config.py
@@ -5,3 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret'
+
